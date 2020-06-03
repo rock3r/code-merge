0c6da3acaa219d72f63f0bf37955cd2d6c69e47e
Sebastiano Poggi <poggos@gmail.com>
2020-06-03 12:47:46 +0200

    Add ktlint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.*
+import dev.sebastiano.codemerge.build.gradle.buildConfigFile
+import dev.sebastiano.codemerge.build.gradle.isCi
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    configurations.classpath.get()
+            .resolutionStrategy.force("com.github.pinterest:ktlint:0.36.0")
+}
 
 plugins {
     kotlin("jvm") version "1.3.72"
+    id("io.gitlab.arturbosch.detekt") version "1.9.1"
+    id("org.jmailen.kotlinter") version "2.3.2"
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 group = "dev.sebastiano"
@@ -9,14 +23,33 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
+val detektVersion = "1.9.1"
+detekt {
+    toolVersion = detektVersion
+    autoCorrect = !isCi()
+    input = files("src/main/java", "src/main/kotlin", "buildSrc/src/main/kotlin")
+    config.from(files(buildConfigFile("detekt/detekt.yml")))
+    buildUponDefaultConfig = true
+}
+
+kotlinter {
+    indentSize = 4
+    reporters = arrayOf("html", "checkstyle", "plain")
+}
+
 tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    withType<JavaCompile> {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
     }
 }
